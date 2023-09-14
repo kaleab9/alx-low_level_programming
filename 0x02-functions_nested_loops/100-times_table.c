74c4d43de0b6d24c5f8736a9f2feb4090a2efc15
kaleab <kaleaba008@gmail.com>
2023-09-14 16:23:38 -0700

    kidist

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,41 @@
 #include "main.h"
 
 /**
+ * print_value - prints a value with appropriate formatting
+ * @value: the value to be printed
+ * @is_first: flag indicating if it is the first value in the row
+ */
+void print_value(int value, int is_first)
+{
+	if (is_first)
+		_putchar(value + '0');
+	else if (value < 10)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(value + '0');
+	}
+	else if (value >= 10 && value < 100)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((value / 10) + '0');
+		_putchar((value % 10) + '0');
+	}
+	else if (value >= 100)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar((value / 100) + '0');
+		_putchar(((value / 10) % 10) + '0');
+		_putchar((value % 10) + '0');
+	}
+}
+
+/**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
@@ -8,44 +43,16 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = i * j;
+	if (n < 0 || n > 15)
+		return;
 
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			print_value(k, j == 0);
 		}
+		_putchar('\n');
 	}
 }
