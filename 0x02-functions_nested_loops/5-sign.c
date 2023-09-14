eda7830c8115ac48a8181942a2b74172f432b482
kaleab <kaleaba008@gmail.com>
2023-09-14 15:50:18 -0700

    kale

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,21 +9,21 @@
  */
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                _putchar('+');
-                return (1);
-        }
-        else if (n == 0)
-        {
-                _putchar('0');
-                return (0);
-        }
-        else if (n < 0)
-        {
-                _putchar('-');
-                return (-1);
-        }
-
-        return (0);  /* Add parentheses around the return value */
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	
+	}
+	return 0;  /* Add this return statement at the end of the function */
 }
